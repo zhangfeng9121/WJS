ff2d8df9e0c60ac812d20e16b8c8efc1e98ca151
zhangfeng <1056859084@qq.com>
2019-05-31 12:56:53 +0800

    touch

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ $(function() {
         var ulWidth = 40;
         $("#cptj .ul-wapper .nav-tabs").children().each(function(index, el) {
             ulWidth += el.clientWidth;
-            console.log(ulWidth);
+            // console.log(ulWidth);
         });
         if(ulWidth > $(window).width()) {
             $("#cptj .ul-wapper .nav-tabs").css({
@@ -48,4 +48,43 @@ $(function() {
 
     // 初始化tooltips
     $('[data-toggle="tooltip"]').tooltip();
+
+    // 新闻标题反写
+    var $newTitle = $('.news-title');
+    $("#news .nav-stacked a").on('click', function(event) {
+        var $this = $(this);
+        var titleText = $this.data('title');
+        $newTitle.text(titleText);
+    });
+
+
+    // 获取界面上的轮播图容器
+    var $co = $('.carousel');
+    var startX,endX;
+    // 灵敏度控制
+    var offset = 50;
+    $co.on('touchstart', function(e) {
+        // console.log(e);
+        startX = e.originalEvent.touches[0].clientX;
+        // console.log('startX' + startX);
+    });
+
+    $co.on('touchmove', function(e) {
+        // console.log(e);
+        endX = e.originalEvent.touches[0].clientX;
+        // console.log('endX' + endX);
+    });
+
+    
+    $co.on('touchend', function(e) {
+        var distance = Math.abs(startX - endX);
+        // console.log(distance);
+        // 触发轮播事件
+        if(offset < distance) {
+            console.log(distance);
+            $(this).carousel(startX > endX ? 'next' : 'prev')
+        }
+    });
+
+    // console.log(startX + '---' + endX);
 });
